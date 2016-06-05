f99333ddc186ce0cff4123fece7fcd7b57d3ca3f
Chris Oliver <excid3@gmail.com>
2016-06-04 20:34:11 -0500

    Move templates and add Rails 4.2 templates

--- lib/go_errors.rb
+++ lib/go_errors.rb
@@ -3,8 +3,9 @@ require "go_errors/middleware"
 require "go_errors/railtie"
 
 # Override the error templates path to serve our modified ones
+rails_version = Rails.version.split(".")[0..1].join(".")
 ActionDispatch::DebugExceptions.send(:remove_const, :RESCUES_TEMPLATE_PATH)
-ActionDispatch::DebugExceptions.const_set(:RESCUES_TEMPLATE_PATH, File.expand_path('../templates', __FILE__))
+ActionDispatch::DebugExceptions.const_set(:RESCUES_TEMPLATE_PATH, File.expand_path("../templates/#{rails_version}", __FILE__))
 
 module GoErrors
   class << self
